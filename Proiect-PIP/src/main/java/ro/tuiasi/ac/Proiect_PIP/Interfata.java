5ee566306bd5ec960ebea1a85a47bbf5a5e35c0b
Kosmaan <radu.cosmin.30.1@gmail.com>
2024-05-25 12:50:43 +0300

    v5

--- Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/Interfata.java
+++ Proiect-PIP/src/main/java/ro/tuiasi/ac/Proiect_PIP/Interfata.java
@@ -1,6 +1,9 @@
 package ro.tuiasi.ac.Proiect_PIP;
 
 import javax.swing.*;
+
+import org.w3c.dom.html.HTMLSelectElement;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +13,8 @@ public class Interfata {
 	public void initInterface() {
 		Citire c = new Citire();
 		c.citireProfesori();
+		c.citireMaterii();
+		c.citireGrupe(1);
 		
 		Scriere s = new Scriere();
 		
@@ -28,25 +33,59 @@ public class Interfata {
 		gbcStanga.insets = new Insets(10, 60, 10, 60); // Adăugăm padding mai mare
 
 // Adăugăm combobox-uri pentru secțiunea stângă
-		String[] itemsStanga = { "Nume Profesor", "Functia", "Asistent" };
+		String[] itemsStanga = { "numeProfesor", "functieProfesor", "numeAsistent" };
 		String[] profi = { "lavinia", "cosmin", "alex" };
-		for (int i = 0; i < itemsStanga.length; i++) {
-			JComboBox comboBox = new JComboBox<>(c.profesori);
-			comboBox.setRenderer(new MyComboBoxRenderer(itemsStanga[i]));
-			comboBox.setSelectedIndex(-1);
-			comboBox.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
-			comboBox.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
-			gbcStanga.gridy = i; // Setăm poziția verticală
-			panelStanga.add(comboBox, gbcStanga);
-			comboBox.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					String selectedValueProfesori = (String) comboBox.getSelectedItem();
-					s.replaceText("numeProfesor", selectedValueProfesori);
-			        System.out.println("Selected Value: " + selectedValueProfesori);
-				}
-			});
-		}
+		String[] functii_profesori = { "Profesor", "Conferentiar", "Sef lucrari", "Asistent" };
+		JComboBox comboBox1 = new JComboBox<>(c.profesori);
+		comboBox1.setRenderer(new MyComboBoxRenderer(itemsStanga[0]));
+		comboBox1.setSelectedIndex(-1);
+		comboBox1.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox1.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcStanga.gridy = 0; // Setăm poziția verticală
+		panelStanga.add(comboBox1, gbcStanga);
+		comboBox1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValueProfesori = (String) comboBox1.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValueProfesori);
+			}
+		});
+		
+		JComboBox comboBox2 = new JComboBox<>(functii_profesori);
+		comboBox2.setRenderer(new MyComboBoxRenderer(itemsStanga[1]));
+		comboBox2.setSelectedIndex(-1);
+		comboBox2.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox2.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcStanga.gridy = 1; // Setăm poziția verticală
+		panelStanga.add(comboBox2, gbcStanga);
+		comboBox2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValueProfesori = (String) comboBox2.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValueProfesori);
+			}
+		});
+		
+		JComboBox comboBox3 = new JComboBox<>(c.profesori);
+		comboBox3.setRenderer(new MyComboBoxRenderer(itemsStanga[2]));
+		comboBox3.setSelectedIndex(-1);
+		comboBox3.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox3.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcStanga.gridy = 2; // Setăm poziția verticală
+		panelStanga.add(comboBox3, gbcStanga);
+		comboBox3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValueProfesori = (String) comboBox3.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValueProfesori);
+			}
+		});
+		
+		JTextField textField1 = new JTextField();
+		textField1.setPreferredSize(new Dimension(300, 50));
+		gbcStanga.gridy = 3;
+		panelStanga.add(textField1, gbcStanga);
+		
 // Crearea celui de-al doilea panel pentru secțiunea dreaptă
 		JPanel panelDreapta = new JPanel(new GridBagLayout()); // Utilizăm GridBagLayout
 		GridBagConstraints gbcDreapta = new GridBagConstraints();
@@ -55,23 +94,58 @@ public class Interfata {
 		gbcDreapta.insets = new Insets(10, 60, 10, 60); // Adăugăm padding mai mare
 
 // Adăugăm combobox-uri pentru secțiunea dreaptă
-		String[] itemsDreapta = { "Grupa", "Laboratorul", "Disciplina" };
-		for (int i = 0; i < itemsDreapta.length; i++) {
-			JComboBox comboBox = new JComboBox<>(profi);
-			comboBox.setRenderer(new MyComboBoxRenderer(itemsDreapta[i]));
-			comboBox.setSelectedIndex(-1);
-			comboBox.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
-			comboBox.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
-			gbcDreapta.gridy = i; // Setăm poziția verticală
-			panelDreapta.add(comboBox, gbcDreapta);
-			comboBox.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					String selectedValue = (String) comboBox.getSelectedItem();
-			        System.out.println("Selected Value: " + selectedValue);
-				}
-			});
-		}
+		String[] itemsDreapta = { "numeGrupe", "numeLaborator", "numeMaterie" };
+		String[] laboratoare = { "A0-1", "A0-2", "A0-3", "A0-4", "A0-5", "A0-6", "A0-7", "A0-8", "A0-9", "A0-10", "A0-11", "A0-12" };
+		JComboBox comboBox4 = new JComboBox<>(c.grupe);
+		comboBox4.setRenderer(new MyComboBoxRenderer(itemsDreapta[0]));
+		comboBox4.setSelectedIndex(-1);
+		comboBox4.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox4.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcDreapta.gridy = 0; // Setăm poziția verticală
+		panelDreapta.add(comboBox4, gbcDreapta);
+		comboBox4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValue = (String) comboBox4.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValue);
+			}
+		});
+		
+		JComboBox comboBox5 = new JComboBox<>(laboratoare);
+		comboBox5.setRenderer(new MyComboBoxRenderer(itemsDreapta[1]));
+		comboBox5.setSelectedIndex(-1);
+		comboBox5.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox5.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcDreapta.gridy = 1; // Setăm poziția verticală
+		panelDreapta.add(comboBox5, gbcDreapta);
+		comboBox5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValue = (String) comboBox5.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValue);
+			}
+		});
+		
+		JComboBox comboBox6 = new JComboBox<>(c.materii);
+		comboBox6.setRenderer(new MyComboBoxRenderer(itemsDreapta[2]));
+		comboBox6.setSelectedIndex(-1);
+		comboBox6.setFont(new Font("Arial", Font.PLAIN, 14)); // Personalizăm fontul
+		comboBox6.setPreferredSize(new Dimension(300, 70)); // Mărim butonul
+		gbcDreapta.gridy = 2; // Setăm poziția verticală
+		panelDreapta.add(comboBox6, gbcDreapta);
+		comboBox6.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedValue = (String) comboBox6.getSelectedItem();
+		        System.out.println("Selected Value: " + selectedValue);
+			}
+		});
+		
+		JTextField textField2 = new JTextField();
+		textField2.setPreferredSize(new Dimension(300, 50));
+		gbcDreapta.gridy = 3;
+		panelDreapta.add(textField2, gbcDreapta);
+		
 		JPanel panelCentral = new JPanel(new GridBagLayout()); // Utilizăm GridBagLayout
 		GridBagConstraints gbcCentral = new GridBagConstraints();
 		gbcCentral.fill = GridBagConstraints.HORIZONTAL; // Fill horizontally
@@ -84,8 +158,28 @@ public class Interfata {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 // Logica pentru salvarea datelor
+				s.replaceText("numeProfesor", (String) comboBox1.getSelectedItem());
+				s.replaceText("functieProfesor", (String) comboBox2.getSelectedItem());
+				s.replaceText("numeAsistent", (String) comboBox3.getSelectedItem());
+				s.replaceText("numeGrupe", (String) comboBox4.getSelectedItem());
+				s.replaceText("numeLaborator", (String) comboBox5.getSelectedItem());
+				s.replaceText("numeMaterie", (String) comboBox6.getSelectedItem());
+				s.replaceText("dataCompletare", textField1.getText());
+				s.replaceText("dataFinala", textField2.getText());
+				if (c.studenti.isEmpty())
+					c.citireStudenti(1, (String) comboBox4.getSelectedItem());
+				int nrStudenti = c.studenti.size();
+				String nrStud = Integer.toString(nrStudenti);
+				s.replaceText("nrStudenti", nrStud);
+				/*
+				s.replaceText("Num_std6", "Imi bag");
+				s.replaceText("Num_std7", "Imi bag00000");
+				s.replaceText("Num_std8", "Imi bacvbsesag");
+				s.replaceText("Num_std9", "Imi bagawd");
+				s.replaceText("Num_std10", "Imi awdwadwabag");
+				*/
+				
 				System.out.println("Datele au fost salvate.");
-
 			}
 		});
 		panelCentral.add(saveButton, gbcCentral); // Adăugăm butonul de salvare mai întâi
